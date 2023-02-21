806416a9fc80e6e309e53d02b9d27413b82d2e6c
sreehari ps <sreehari.ps@aot-technologies.com>
2023-02-21 11:33:27 +0530

    Add themes utility module
    Themes will contain css
    variables for theming.
    
    Themes will contain all
    the global css for
    formsflow.ai

---
+++ forms-flow-theme/src/formsflow-theme.js
@@ -0,0 +1,3 @@
+// Anything exported from this file is importable by other in-browser modules.
+import "./global.css?modules=false";
+export function publicApiFunction() {}
---
+++ forms-flow-theme/webpack.config.js
@@ -0,0 +1,36 @@
+const { merge } = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "formsflow",
+    projectName: "theme",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      port: 8082
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+  });
+};
