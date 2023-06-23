574ec7f64764e83d682ebe51446e37f5047ce4c9
abhilash-aot <abhilash.kr@aot-technologies.com>
2023-06-23 04:57:45 -0700

    Updated show application Env

--- forms-flow-nav/src/constants/userContants.js
+++ forms-flow-nav/src/constants/userContants.js
@@ -3,16 +3,30 @@
 import { GROUPS } from "./groupConstants";
 export const REVIEWER_GROUP = "formsflow/formsflow-reviewer";
 
-export const defaultUserAccessGroupCheck = {
-  accessAllowApplications: false,
-  accessAllowSubmissions: false,
-};
-let userAccessGroupCheck =
-  (window._env_ && window._env_.REACT_APP_USER_ACCESS_PERMISSIONS) ||
-  defaultUserAccessGroupCheck;
+/****
+ * Default value of REACT_APP_ENABLE_APPLICATION_ACCESS_PERMISSION_CHECK is false
+ * This is to check if the Application tab is to be shown with respect to group info or not
+ *
+ * Currently added groups for the purpose are applicationsAccess:
+ * ["/formsflow/formsflow-reviewer/access-allow-applications",
+ * "/formsflow/formsflow-client/access-allow-applications"]
+ *  ****/
+//TODO Make this function Common
+function getEnv(env_string) {
+  let ENV_BOOLEAN =
+      (window._env_ && window._env_["REACT_APP_ENABLE_APPLICATION_ACCESS_PERMISSION_CHECK"]) ||
+      process.env[env_string] ||
+      false;
+
+  return ENV_BOOLEAN === "true" || ENV_BOOLEAN === true
+      ? true
+      : false;
+}
+
+let userAccessGroupCheckforApplications =getEnv('REACT_APP_ENABLE_APPLICATION_ACCESS_PERMISSION_CHECK');
 
 export const setShowApplications = (userGroups) => {
-  if (!userAccessGroupCheck.accessAllowApplications) {
+  if (!userAccessGroupCheckforApplications) {
     return true;
   } else if (userGroups?.length) {
     const applicationAccess = GROUPS.applicationsAccess.some((group) =>
