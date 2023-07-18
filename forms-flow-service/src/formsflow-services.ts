11f949049c01303c2cd11166efb44f55dc6bb5e7
shuhaib-aot <shuhaib.s@aot-technologies.com>
2023-07-18 15:02:03 +0530

    Changed internationalization

--- forms-flow-service/src/formsflow-services.ts
+++ forms-flow-service/src/formsflow-services.ts
@@ -1,5 +1,6 @@
 import KeycloakService from "./keycloak/KeycloakService";
 import StorageService from "./storage/storageService";
 import RequestService from "./request/requestService";
+import i18nService from "./resourceBundles/i18n";
 
-export { KeycloakService, StorageService, RequestService };
+export { KeycloakService, StorageService, RequestService, i18nService };
---
+++ forms-flow-service/src/resourceBundles/i18n.ts
@@ -0,0 +1,81 @@
+import { createInstance, Resource } from "i18next"; 
+import { RESOURCE_BUNDLES_ZH } from "./zh/resourceBundles";
+import { RESOURCE_BUNDLES_EN } from "./en/resourceBundles";
+import { RESOURCE_BUNDLES_FR } from "./fr/resourceBundles";
+import { RESOURCE_BUNDLES_PT } from "./pt/resourceBundles";
+import { RESOURCE_BUNDLES_BG } from "./bg/resourceBundles";
+import { RESOURCE_BUNDLES_DE } from "./de/resourceBundles";
+
+interface CustomResourceBundle {
+  [key: string]: Resource;
+}
+
+class I18nManager {
+  private i18nInstance: any;
+  constructor() {
+    const resources: CustomResourceBundle = {
+      en: {
+        translation: RESOURCE_BUNDLES_EN,
+      },
+      "zh-CN": {
+        translation: RESOURCE_BUNDLES_ZH,
+      },
+      pt: {
+        translation: RESOURCE_BUNDLES_PT,
+      },
+      fr: {
+        translation: RESOURCE_BUNDLES_FR,
+      },
+      bg: {
+        translation: RESOURCE_BUNDLES_BG,
+      },
+      de: {
+        translation: RESOURCE_BUNDLES_DE,
+      },
+    };
+    
+    this.i18nInstance = createInstance({
+      resources,
+    }, (err: Error, t: any) => {
+      if (err) {
+        console.error(err);
+      } else {
+        this.checkCustomResourceBundleAndUpdate();
+      }
+    });
+  }
+
+  private async checkCustomResourceBundleAndUpdate() {
+    const envValue: any = (window as any)._env_;
+    if (envValue.REACT_APP_CUSTOM_RESOURCE_BUNDLE) {
+      try {
+        const response = await fetch(envValue.REACT_APP_CUSTOM_RESOURCE_BUNDLE);
+        const data = await response.json();
+
+        Object.keys(data).forEach((key) => {
+          this.updateResourceBundle(key, data[key]);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  public updateResourceBundle(language: any, newTranslations: any) {
+    this.i18nInstance.addResourceBundle(
+      language,
+      "translation",
+      newTranslations,
+      true,
+      true
+    );
+  }
+
+  public getInstance() {
+    return this.i18nInstance;
+  }
+}
+
+const i18nManager = new I18nManager();
+export default i18nManager.getInstance();
+
