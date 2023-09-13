d6480a593b61223925e6e8d119ea6dff5b29e70f
Ajay krishna <ajay.k@aot-technologies.com>
2023-09-13 13:55:28 +0530

    removed the unwanted files

--- forms-flow-service/src/formsflow-services.ts
+++ forms-flow-service/src/formsflow-services.ts
@@ -3,6 +3,5 @@ import StorageService from "./storage/storageService";
 import RequestService from "./request/requestService";
 import i18nService from "./resourceBundles/i18n";
 import HelperServices from "./helpers/helperServices";
-import JsonService from "./customTheme/customTheme"
 
-export { KeycloakService, StorageService, RequestService, i18nService, HelperServices, JsonService};
+export { KeycloakService, StorageService, RequestService, i18nService, HelperServices,};
