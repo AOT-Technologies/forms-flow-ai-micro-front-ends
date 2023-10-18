b7dc4d563c918ac63bbc3aa3e209cd8c201e7d23
Abijith Sebastian <98075058+AbijithS-aot@users.noreply.github.com>
2023-10-18 10:50:35 -0700

    Delete forms-flow-service/src/constants/constant.ts
    removed duplicate file

--- forms-flow-service/src/constants/constant.ts
+++
@@ -1,5 +0,0 @@
-export const DATE_FORMAT =
-  (window as any)._env_ && (window as any)._env_.REACT_APP_DATE_FORMAT || "DD-MM-YYYY";
-
-export const TIME_FORMAT =
-(window as any)._env_ && (window as any)._env_.REACT_APP_TIME_FORMAT || "hh:mm:ss A";
