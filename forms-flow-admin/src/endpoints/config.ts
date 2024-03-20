d39f53502d690c1e02d45e977c0d5de3456d90a4
Sumesh Punakkal Kariyil <sumesh.pk@aot-technologies.com>
2024-03-20 15:46:50 -0700

    Changes to add custom Keycloak relative path

--- forms-flow-admin/src/endpoints/config.ts
+++ forms-flow-admin/src/endpoints/config.ts
@@ -5,6 +5,8 @@ declare global {
   }
 export const WEB_BASE_URL = (window._env_ && window._env_.REACT_APP_WEB_BASE_URL) 
 export const KEYCLOAK_URL = (window._env_ && window._env_.REACT_APP_KEYCLOAK_URL) 
-export const KEYCLOAK_URL_AUTH = `${KEYCLOAK_URL}/auth`
+export const KEYCLOAK_URL_HTTP_RELATIVE_PATH = (window._env_ && window._env_.REACT_APP_KEYCLOAK_URL_HTTP_RELATIVE_PATH) || '/auth';
+export const KEYCLOAK_URL_AUTH = `${KEYCLOAK_URL}${KEYCLOAK_URL_HTTP_RELATIVE_PATH}`;
+
 export const KEYCLOAK_URL_REALM = (window._env_ && window._env_.REACT_APP_KEYCLOAK_URL_REALM) 
 export const KEYCLOAK_CLIENT = (window._env_ && window._env_.REACT_APP_KEYCLOAK_CLIENT) 
