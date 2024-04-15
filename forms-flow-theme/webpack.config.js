385edbcfdec25ff6fab1802f41eec09b859f3574
shuhaib-aot <shuhaib.s@aot-technologies.com>
2024-04-15 11:13:15 +0530

    webapack changed to css form js

--- forms-flow-theme/webpack.config.js
+++ forms-flow-theme/webpack.config.js
@@ -1,39 +1,37 @@
-const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = (webpackConfigEnv, argv) => {
-  const defaultConfig = singleSpaDefaults({
-    orgName: "formsflow",
-    projectName: "theme",
-    webpackConfigEnv,
-    argv,
-  });
-
-  return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
-    devServer: {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
+module.exports = {
+  entry: "./src/main.scss", // Entry point for your SCSS file
+  output: {
+    // Output directory for the generated CSS file
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    port: 3008
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Extracts CSS into separate files
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
-      port: 3008
-    },
-    output:{
-      filename:"forms-flow-theme.js"
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-      ],
-    },
-  });
+    ],
+  },
+  plugins: [
+    // Plugin to extract CSS into separate files
+    new MiniCssExtractPlugin({
+      filename: "forms-flow-theme.min.css", // Output minified CSS filename
+    }),
+  ],
 };
