3dac9a9d30d9c984285f2daac57d9d6d24e47188
shuhaib-aot <shuhaib.s@aot-technologies.com>
2024-04-16 14:59:05 +0530

    files added

--- forms-flow-theme/webpack.config.js
+++ forms-flow-theme/webpack.config.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-// const TerserPlugin = require("terser-webpack-plugin");
-
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
 
