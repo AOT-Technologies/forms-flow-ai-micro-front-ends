85f8a63cc175d3855ffa88c52108496cfdf5b8d7
Ajay krishna <ajay.k@aot-technologies.com>
2024-08-01 23:23:48 +0530

    custom button styles added and sidenav new changes added

--- forms-flow-nav/src/sidenav/MenuComponent.jsx
+++ forms-flow-nav/src/sidenav/MenuComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 import "./Sidebar.scss";
-import { Link, useLocation, useHistory  } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import ChevronIcon from "./chevronicon.svg";
 import { MULTITENANCY_ENABLED } from "../constants/constants";
 import { useTranslation } from "react-i18next";
@@ -12,50 +12,58 @@ const MenuComponent = ({ eventKey, mainMenu, subMenu, optionsCount }) => {
   const baseUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantKey}/` : "/";
   const { t } = useTranslation();
   const noOptionsMenu = optionsCount === "0";
+
   const handleHeaderClick = () => {
     if (noOptionsMenu) {
-      const taskMenu = subMenu.find(menu => menu.path === "task");
-      if (taskMenu) {
-        history.push(`${baseUrl}${taskMenu.path}`);
-      }
+      subMenu.map((item, index) => {
+        history.push(`${baseUrl}${item.path}`);
+      });
     }
   };
 
   return (
     <Accordion.Item eventKey={eventKey}>
-      <Accordion.Header 
-        data-testid={`accordion-header-${eventKey}`} 
-        aria-label={`Accordion header for ${mainMenu}`} 
+      <Accordion.Header
+        data-testid={`accordion-header-${eventKey}`}
+        aria-label={`Accordion header for ${mainMenu}`}
         className={`${noOptionsMenu ? "no-arrow" : ""} ${
-          noOptionsMenu && subMenu.some(menu => menu.matchExp && menu.matchExp.test(location.pathname)) ? "active" : ""
+          noOptionsMenu &&
+          subMenu.some((menu) =>
+            menu.matchExps &&
+            menu.matchExps.some((exp) => exp.test(location.pathname))
+          )
+            ? "active"
+            : ""
         }`}
         onClick={noOptionsMenu ? handleHeaderClick : undefined}
-        >
-          {!noOptionsMenu && (
-        <img src={ChevronIcon} alt="Chevron icon" className="custom-chevron" />
-          )}
-        <span>{mainMenu}</span>
+      >
+        {!noOptionsMenu && (
+          <img
+            src={ChevronIcon}
+            alt="Chevron icon"
+            className="custom-chevron"
+          />
+        )}
+        <span>{t(mainMenu)}</span>
       </Accordion.Header>
       {!noOptionsMenu && (
-      <Accordion.Body>
-        {subMenu.map((menu, index) => (
-          <Link
-            key={index}
-            to={`${baseUrl}${menu.path}`}
-            className={`accordion-link ${
-              menu.matchExp
-                ? menu.matchExp.test(location.pathname)
+        <Accordion.Body>
+          {subMenu.map((menu, index) => (
+            <Link
+              key={index}
+              to={`${baseUrl}${menu.path}`}
+              className={`accordion-link ${
+                menu.matchExps && menu.matchExps.some((exp) => exp.test(location.pathname))
                   ? "active"
                   : ""
-                : ""
-            } `}
-            data-testid={`accordion-link-${index}`}
-            aria-label={`Link to ${menu.name}`}
-          >
-            {menu.name}
-          </Link>
-        ))}
-      </Accordion.Body>
+              } `}
+              data-testid={`accordion-link-${index}`}
+              aria-label={`Link to ${menu.name}`}
+            >
+              {t(menu.name)}
+            </Link>
+          ))}
+        </Accordion.Body>
       )}
     </Accordion.Item>
   );
