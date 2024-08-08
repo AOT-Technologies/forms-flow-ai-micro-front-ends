02d98931f8e02a9569197d82e37edf2b7ec8eabc
shuhaib-aot <shuhaib.s@aot-technologies.com>
2024-08-08 18:41:07 +0530

    FWF-3573 [Feature] Added component service initially

---
+++ forms-flow-components/src/components/SvgIcons/index.tsx
@@ -0,0 +1,14 @@
+ 
+export const ArrowIcon = ({ color = 'currentColor', ...props }) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill={color}
+      {...props}
+    >
+      <circle cx="12" cy="12" r="10" />
+    </svg>
+  );
+
