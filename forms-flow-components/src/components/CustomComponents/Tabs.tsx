6f78593cd1cef912af8a01d1cf1893963aed9ed3
DESKTOP-E41MSIT\saneesh <124969366+saneesh-aot@users.noreply.github.com>
2024-09-04 05:36:01 -0700

    Added reusable components and updated styles

---
+++ forms-flow-components/src/components/CustomComponents/Tabs.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+
+interface TabItem {
+  eventKey: string;
+  title: string;
+  content: React.ReactNode;
+}
+
+interface CustomTabsProps {
+  defaultActiveKey: string;
+  id?: string;
+  tabs: TabItem[];
+  dataTestid?: string;
+  ariaLabel?: string;
+  onSelect?: (eventKey: string | null) => void;
+}
+
+export const CustomTabs: React.FC<CustomTabsProps> = ({
+  defaultActiveKey,
+  id = "custom-tab",
+  tabs,
+  dataTestid = "",
+  ariaLabel = "",
+  onSelect,
+}) => {
+  return (
+    <Tabs
+      defaultActiveKey={defaultActiveKey}
+      id={id}
+      className="custom-tabs mb-3"
+      data-testid={dataTestid}
+      aria-label={ariaLabel}
+      onSelect={onSelect}
+    >
+      {tabs.map((tab, index) => (
+        <Tab key={index} eventKey={tab.eventKey} title={tab.title}>
+          {tab.content}
+        </Tab>
+      ))}
+    </Tabs>
+  );
+};
--- forms-flow-components/src/components/index.ts
+++ forms-flow-components/src/components/index.ts
@@ -1,2 +1,5 @@
 export * from "./SvgIcons";
 export * from "./CustomComponents/Button";
+export * from "./CustomComponents/Pills";
+export * from "./CustomComponents/Tabs";
+export * from "./CustomComponents/Radio";
