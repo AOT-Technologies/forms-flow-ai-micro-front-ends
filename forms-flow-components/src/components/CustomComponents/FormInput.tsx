e303937f33fcc35a359a8e51a240428a4b27e1ce
fahad-aot <fahad.k@aot-technologies.com>
2024-09-08 11:43:04 -0700

    Added inputBox in components

---
+++ forms-flow-components/src/components/CustomComponents/FormInput.tsx
@@ -0,0 +1,82 @@
+import React, { ChangeEvent, FocusEvent } from 'react';
+import { Form, InputGroup } from 'react-bootstrap';
+
+interface FormInputProps {
+  type?: string;
+  label?: string;
+  value?: string;
+  onChange?: () => void;
+  onBlur?: () => void;
+  placeholder?: string;
+  isInvalid?: boolean;
+  feedback?: string;
+  disabled?: boolean;
+  size?: 'sm' | 'lg';
+  dataTestid?: string;
+  ariaLabel?: string;
+  className?: string;
+  required?: boolean;
+  icon?: React.ReactNode;
+  id?: string;
+}
+
+export const FormInput: React.FC<FormInputProps> = ({
+  type = "text",
+  label,
+  value ,
+  onChange,
+  onBlur,
+  placeholder = "",
+  isInvalid = false,
+  feedback = "",
+  disabled = false,
+  size,
+  dataTestid,
+  ariaLabel,
+  className = '',
+  required = false,
+  icon,
+  id
+}) => {
+
+  const inputClassNames = `form-control-input ${className}`;
+
+  return (
+    <div className="form-input-box">
+      <Form.Group controlId={id}>
+        {label && (
+          <Form.Label className='custom-form-control-label'>
+            {label} {required && <span className='required-icon'>*</span>}
+          </Form.Label>
+        )}
+        <InputGroup className="custom-form-input-group">
+          <Form.Control
+            type={type}
+            value={value}
+            onChange={onChange}
+            onBlur={onBlur}
+            placeholder={placeholder}
+            isInvalid={isInvalid}
+            disabled={disabled}
+            size={size}
+            data-testid={dataTestid}
+            aria-label={ariaLabel}
+            required={required}
+            className={inputClassNames}
+          />
+          {icon && (
+            <InputGroup.Text id="basic-addon1" className={disabled ? 'disabled-icon' : ''}>
+              {icon}
+            </InputGroup.Text>
+          )}
+          {isInvalid && (
+            <Form.Control.Feedback className='custom-feedback' type="invalid">
+              {feedback}
+            </Form.Control.Feedback>
+          )}
+        </InputGroup>
+      </Form.Group>
+    </div>
+  );
+};
+
