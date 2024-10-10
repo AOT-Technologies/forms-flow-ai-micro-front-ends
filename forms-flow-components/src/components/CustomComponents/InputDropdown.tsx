46884d1b875bdd4b8f888a4393db1e2eb517160a
fahad-aot <fahad.k@aot-technologies.com>
2024-10-10 08:06:01 -0700

    inputdropdown updated

--- forms-flow-components/src/components/CustomComponents/InputDropdown.tsx
+++ forms-flow-components/src/components/CustomComponents/InputDropdown.tsx
@@ -6,9 +6,9 @@ import { CloseIcon , ChevronIcon } from "../SvgIcons/index";
 
 
 interface DropdownItem {
-    label: string;
-    onClick:()=>void;
-  }
+  label: string;
+  onClick: () => void;
+}
 interface InputDropdownProps {
   Options: DropdownItem[];
   firstItemLabel: string;
@@ -16,128 +16,137 @@ interface InputDropdownProps {
   placeholder?: string;
   isAllowInput: boolean;
   required?: boolean;
-  value?:string;
+  value?: string;
+  selectedOption?: string; 
 }
 
 export const InputDropdown: React.FC<InputDropdownProps> = ({
-  Options= [],
+  Options = [],
   firstItemLabel,
   dropdownLabel,
   placeholder = '',
   isAllowInput,
-  required = false
+  required = false,
+  selectedOption // Use the new selectedOption prop
 }) => {
-  const [selectedItem, setSelectedItem] = useState<DropdownItem | undefined>(undefined);
+  const [selectedItem, setSelectedItem] = useState<DropdownItem | undefined>(); // Initialize with selectedOption
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
-  const [inputValue, setInputValue] = useState<string>(selectedItem?.label || '');
+  const [inputValue, setInputValue] = useState<string>(selectedOption || ''); // Initialize inputValue with selectedOption's label
   const [filteredItems, setFilteredItems] = useState<DropdownItem[]>([]);
   const [textBoxInput, setTextBoxInput] = useState<boolean>(false);
 
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const toggleDropdown = () => {
-    setIsDropdownOpen((prev) => !prev);
+      setIsDropdownOpen((prev) => !prev);
   };
 
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-        setIsDropdownOpen(false);
-      }
-    };
+      const handleClickOutside = (event: MouseEvent) => {
+          if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+              setIsDropdownOpen(false);
+          }
+      };
 
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+      };
   }, [dropdownRef]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    setInputValue(value);
+  useEffect(() => {
+      
+      if (selectedOption) {
+          setInputValue(selectedOption);
+      }
+  }, [selectedOption]);
 
-    const filtered = Options.filter((item) =>
-      item.label.toLowerCase().includes(value.toLowerCase())
-    );
-    setFilteredItems(filtered);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      setInputValue(value);
+
+      const filtered = Options.filter((item) =>
+          item.label.toLowerCase().includes(value.toLowerCase())
+      );
+      setFilteredItems(filtered);
   };
 
   const handleSelect = (item: DropdownItem) => {
-    setSelectedItem(item);
-    setInputValue(item.label);
-    setIsDropdownOpen(false);
-    if (item.onClick) {
-        item.onClick(); 
+      setSelectedItem(item);
+      setInputValue(item.label);
+      setIsDropdownOpen(false);
+      if (item.onClick) {
+          item.onClick(); 
       }
   };
 
   const onFirstItemClick = () => {
-    setTextBoxInput(true);
-    setInputValue('');
-    setSelectedItem(undefined);
-    setIsDropdownOpen(false);
+      setTextBoxInput(true);
+      setInputValue('');
+      setSelectedItem(undefined);
+      setIsDropdownOpen(false);
   };
 
   const handleClose = () => {
-    setTextBoxInput(false);
-    setInputValue('');
-    setIsDropdownOpen(true);
+      setTextBoxInput(false);
+      setInputValue('');
+      setIsDropdownOpen(true);
   };
 
   return (
-    <div ref={dropdownRef} className="input-dropdown">
-      {textBoxInput ? (
-        <InputGroup>
-          <FormInput
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            aria-label="category input"
-            icon={<CloseIcon  onClick={handleClose}/>} 
-            className="input-with-close"
-            label={dropdownLabel}
-          />
-        </InputGroup>
-      ) : (
-        <InputGroup>
-          <FormInput
-            placeholder={placeholder}
-            value={inputValue}
-            onChange={handleInputChange}
-            onClick={toggleDropdown}
-            aria-label="Dropdown input"
-            icon={<ChevronIcon />}
-            className={`${isDropdownOpen ? 'border-input collapsed' : ''}`}
-            onIconClick={toggleDropdown}
-            label={dropdownLabel}
-            required ={ required ? true :false}
-          />
-        </InputGroup>
-      )}
-
-      {!textBoxInput && isDropdownOpen && (
-        <ListGroup>
-          {isAllowInput && (
-            <ListGroup.Item
-              onClick={onFirstItemClick}
-              className="list-first-item-btn"
-              data-testid="list-first-item"
-            >
-              {firstItemLabel}
-            </ListGroup.Item>
+      <div ref={dropdownRef} className="input-dropdown">
+          {textBoxInput ? (
+              <InputGroup>
+                  <FormInput
+                      value={inputValue}
+                      onChange={(e) => setInputValue(e.target.value)}
+                      aria-label="category input"
+                      icon={<CloseIcon onClick={handleClose} />} 
+                      className="input-with-close"
+                      label={dropdownLabel}
+                  />
+              </InputGroup>
+          ) : (
+              <InputGroup>
+                  <FormInput
+                      placeholder={placeholder}
+                      value={inputValue}
+                      onChange={handleInputChange}
+                      onClick={toggleDropdown}
+                      aria-label="Dropdown input"
+                      icon={<ChevronIcon />}
+                      className={`${isDropdownOpen ? 'border-input collapsed' : ''}`}
+                      onIconClick={toggleDropdown}
+                      label={dropdownLabel}
+                      required={required}
+                  />
+              </InputGroup>
           )}
-          {(filteredItems.length > 0 ? filteredItems : Options).map((item, index) => (
-            <ListGroup.Item
-              key={index}
-              onClick={() => handleSelect(item)}
-              data-testid={`list-${index}-item`}
-              aria-label={`list-${index}-item`}
-            >
-              {item.label}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-      )}
-    </div>
+
+          {!textBoxInput && isDropdownOpen && (
+              <ListGroup>
+                  {isAllowInput && (
+                      <ListGroup.Item
+                          onClick={onFirstItemClick}
+                          className="list-first-item-btn"
+                          data-testid="list-first-item"
+                      >
+                          {firstItemLabel}
+                      </ListGroup.Item>
+                  )}
+                  {(filteredItems.length > 0 ? filteredItems : Options).map((item, index) => (
+                      <ListGroup.Item
+                          key={index}
+                          onClick={() => handleSelect(item)}
+                          data-testid={`list-${index}-item`}
+                          aria-label={`list-${index}-item`}
+                      >
+                          {item.label}
+                      </ListGroup.Item>
+                  ))}
+              </ListGroup>
+          )}
+      </div>
   );
 };
 
