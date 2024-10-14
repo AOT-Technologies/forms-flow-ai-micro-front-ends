72e550baa1c6a43c038a4c54181cdd750df7e422
fahad-aot <fahad.k@aot-technologies.com>
2024-10-14 07:01:29 -0700

    feature/FWF-3682 added style for save template modal

--- forms-flow-components/src/components/CustomComponents/FormBuilderModal.tsx
+++ forms-flow-components/src/components/CustomComponents/FormBuilderModal.tsx
@@ -120,7 +120,7 @@ export const FormBuilderModal: React.FC<BuildFormModalProps> = React.memo(
               onChange={(event) => {
                 setFormDescription(event.target.value); // Set the description state
               }}
-              minRows={5}
+              minRows={1}
             />
           </Modal.Body>
           <Modal.Footer className="d-flex justify-content-start">
