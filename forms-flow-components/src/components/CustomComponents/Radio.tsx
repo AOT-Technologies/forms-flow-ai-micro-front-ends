f04e814c6e6a06c037bc8e356e72f69e8b31449a
DESKTOP-E41MSIT\saneesh <124969366+saneesh-aot@users.noreply.github.com>
2024-10-15 03:57:48 -0700

    Radio button change defaultChecked index value

--- forms-flow-components/src/components/CustomComponents/Radio.tsx
+++ forms-flow-components/src/components/CustomComponents/Radio.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 
+
 interface RadioOption {
     label: string;
     onClick: () => void;
   }
-  
+ 
   interface CustomRadioButtonProps {
     items: RadioOption[];
     dataTestid?: string;
     ariaLabel?: string;
+    indexValue?: number;
   }
+
 
 export const CustomRadioButton: React.FC<CustomRadioButtonProps> = ({
   items,
   dataTestid = "",
   ariaLabel = "",
+  indexValue = 0,
 }) => {
   return (
-    <Form className="custom-radio-button"  aria-label={ariaLabel}>
+    <Form className="custom-radio-button mb-3"  aria-label={ariaLabel}>
       {items.map((option, index) => (
         <Form.Check
           inline
@@ -29,11 +33,16 @@ export const CustomRadioButton: React.FC<CustomRadioButtonProps> = ({
           id={`inline-radio-${index + 1}`}
           data-testid={`${dataTestid}-inline-radio-${index + 1}`}
           key={`radio-${index + 1}`}
-          defaultChecked={index === 0}
+          defaultChecked={index === indexValue}
           onClick={option.onClick}
+         
         />
       ))}
     </Form>
   );
 };
+
+
+
+
 
