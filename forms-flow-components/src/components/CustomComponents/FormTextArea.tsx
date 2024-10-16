c2ac509e98fd6a425ab1cd1b4144b7f244254841
fahad-aot <fahad.k@aot-technologies.com>
2024-10-16 03:08:56 -0700

    Updated Formtextarea component

--- forms-flow-components/src/components/CustomComponents/FormTextArea.tsx
+++ forms-flow-components/src/components/CustomComponents/FormTextArea.tsx
@@ -21,6 +21,9 @@ interface FormTextAreaProps {
     minRows?: number;
     onIconClick?: () => void;
     maxRows?: number;
+    iconTop?: boolean;
+    iconCenter?: boolean;
+    iconBottom?: boolean;
 }
 
 export const FormTextArea = forwardRef<HTMLTextAreaElement, FormTextAreaProps>(({
@@ -42,6 +45,9 @@ export const FormTextArea = forwardRef<HTMLTextAreaElement, FormTextAreaProps>((
     minRows = 1,
     onIconClick,
     maxRows = 5,
+    iconTop,
+    iconBottom,
+    iconCenter 
 }, ref) => {
     const internalRef = useRef<HTMLTextAreaElement>(null);
     const combinedRef = (ref || internalRef) as React.RefObject<HTMLTextAreaElement>;
@@ -60,6 +66,14 @@ export const FormTextArea = forwardRef<HTMLTextAreaElement, FormTextAreaProps>((
         }
     };
 
+    const iconPositionClass = iconTop
+    ? 'icon-top'
+    : iconCenter
+    ? 'icon-center'
+    : iconBottom
+    ? 'icon-bottom'
+    : 'icon-center';
+    
     return (
         <div className="form-input-box">
             <Form.Group controlId={id}>
@@ -90,7 +104,7 @@ export const FormTextArea = forwardRef<HTMLTextAreaElement, FormTextAreaProps>((
                         <InputGroup.Text
                             id="basic-addon1"
                             onClick={onIconClick}
-                            className={disabled ? 'disabled-icon' : ''}
+                            className={`icon-wrapper  ${iconPositionClass} ${disabled ? 'disabled-icon' : ''}`}
                         >
                             {icon}
                         </InputGroup.Text>
