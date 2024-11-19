df7c8739792dcd7d65cd04ef5123cf55f3f6bc21
Bonymol Baby <88372115+Bonymol-aot@users.noreply.github.com>
2024-11-18 20:58:42 -0800

    typo fix

--- forms-flow-admin/src/components/roles/roles.tsx
+++ forms-flow-admin/src/components/roles/roles.tsx
@@ -27,7 +27,6 @@ import { DEFAULT_ROLES } from "../../constants";
 
 import {removingTenantId} from "../../utils/utils.js";
 import { TableFooter } from "@formsflow/components";
-
 const Roles = React.memo((props: any) => {
   const { t } = useTranslation();
   const { tenantId } = useParams();
@@ -637,7 +636,6 @@ const Roles = React.memo((props: any) => {
     setActivePage(1); 
   };
 
-
   const columns = [
     {
       dataField: "name",
@@ -778,15 +776,14 @@ const Roles = React.memo((props: any) => {
     
           <table className="table">
             <tfoot>
-
-              <TableFooter
-                limit={sizePerPage}
-                activePage={activePage}
-                totalCount={roles.length}
-                handlePageChange={setActivePage}
-                onLimitChange={handleLimitChange}
-                pageOptions={getpageList()}
-              />
+            <TableFooter
+            limit={sizePerPage}
+            activePage={activePage}
+            totalCount={roles.length}
+            handlePageChange={handlePageChange}
+            onLimitChange={handleLimitChange}
+            pageOptions={getPageList()}
+          />
             </tfoot>
           </table>
         </div>
