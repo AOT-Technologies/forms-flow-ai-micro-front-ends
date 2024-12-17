6c32ece54a70d7bfd23bbd57ce73fb21d4e8e4b4
Bonymol-aot <bonymol@aot-technologies.com>
2024-12-17 02:17:43 -0800

    [BugFix]: FWF-4100 Added premium icon color change for selected menu

--- forms-flow-components/src/components/CustomComponents/ShowPremiumIcon.tsx
+++ forms-flow-components/src/components/CustomComponents/ShowPremiumIcon.tsx
@@ -1,11 +1,16 @@
+// ShowPremiumIcons.tsx
 import React from 'react';
-import { SHOW_PREMIUM_ICON} from '../../constants/constants'
-import { StarPremiumIcon } from '../SvgIcons/index'
+import { SHOW_PREMIUM_ICON } from '../../constants/constants';
+import { StarPremiumIcon } from '../SvgIcons/index';
 
-export const ShowPremiumIcons: React.FC = () => {
+interface ShowPremiumIconsProps {
+  color: string;
+}
+
+export const ShowPremiumIcons: React.FC<ShowPremiumIconsProps> = ({ color }) => {
   return (
     <>
-    {SHOW_PREMIUM_ICON && <StarPremiumIcon />}
+      {SHOW_PREMIUM_ICON && <StarPremiumIcon color={color} />} 
     </>
   );
 };
