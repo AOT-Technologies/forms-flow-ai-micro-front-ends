f85531630b8d8c4f685c7ecc8a333b1d6f79060f
fahad-aot <fahad.k@aot-technologies.com>
2025-01-22 07:54:27 -0800

    bugfix/fwf-4280:Added styles for multiselect

--- forms-flow-components/src/components/CustomComponents/MultiSelect.tsx
+++ forms-flow-components/src/components/CustomComponents/MultiSelect.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState,useRef,useEffect } from "react";
 import Multiselect from 'multiselect-react-dropdown';
+import { CloseIcon } from "../SvgIcons/index";
+import { StyleServices } from "@formsflow/service";
 
 
 interface MultiSelectInterface {
@@ -9,6 +11,11 @@ interface MultiSelectInterface {
   onRemove?: (removed: any) => void; 
   displayValue?: string;  
   avoidHighlightFirstOption?: boolean;  
+  hidePlaceholder?:boolean;
+  className?:string;
+  disable?:boolean;
+  placeholder?:string;
+  label?:string;
 }
 
 export const MultipleSelect: React.FC<MultiSelectInterface> = ({
@@ -17,15 +24,60 @@ export const MultipleSelect: React.FC<MultiSelectInterface> = ({
     onSelect = ()=> {},
     onRemove = ()=> {},
     displayValue= "",
-    avoidHighlightFirstOption = true
+    avoidHighlightFirstOption = true,
+    hidePlaceholder=true,
+    className,
+    disable=false,
+    placeholder="",
+    label
 
 })=>{
-    return <Multiselect
-    options={options}  
-    selectedValues={selectedValues} 
-    onSelect={onSelect}  
-    onRemove={onRemove}  
-    displayValue={displayValue}
-    avoidHighlightFirstOption={avoidHighlightFirstOption}
-    />
+   const primaryColor = StyleServices.getCSSVariable('--ff-primary');
+   const dropdownRef = useRef<HTMLDivElement | null>(null);
+   const [isOpen,setIsOpen] = useState(false);
+     // Toggle dropdown open/close when clicked
+  const handleClick = (e: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      dropdownRef.current.contains(e.target as Node)
+    ) {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+   
+  return (
+      <div className={`multiselect-container ${className}`} ref={dropdownRef}>
+       {label && <label className="multiple-select-label">{label}</label>}
+        <Multiselect
+          options={options}
+          selectedValues={selectedValues}
+          className={`${isOpen && "open-dropdown"}`}
+          onSelect={onSelect}
+          onRemove={onRemove}
+          displayValue={displayValue}
+          avoidHighlightFirstOption={avoidHighlightFirstOption}
+          hidePlaceholder={hidePlaceholder}
+          disable={disable}
+          placeholder={placeholder}
+          customCloseIcon={
+            <CloseIcon
+              onClick={onRemove}
+              color={primaryColor}
+              data-testid="pill-remove-icon"
+              aria-label="remove "
+            />
+          }
+        />
+      </div>
+    );
 }
