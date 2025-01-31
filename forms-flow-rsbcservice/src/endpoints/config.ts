b4abfe7e48f410b6d669529f8d8f7b440c339e74
divyav-aot <divyav-aot@github>
2025-01-31 13:14:30 -0500

    cleanedup and removed unnecessary codes

--- forms-flow-rsbcservice/src/endpoints/config.ts
+++ forms-flow-rsbcservice/src/endpoints/config.ts
@@ -4,16 +4,6 @@ declare global {
   }
 }
 export const WEB_BASE_URL = window._env_ && window._env_.REACT_APP_WEB_BASE_URL;
-export const KEYCLOAK_URL = window._env_ && window._env_.REACT_APP_KEYCLOAK_URL;
-export const KEYCLOAK_URL_HTTP_RELATIVE_PATH =
-  (window._env_ && window._env_.REACT_APP_KEYCLOAK_URL_HTTP_RELATIVE_PATH) ||
-  "/auth";
-export const KEYCLOAK_URL_AUTH = `${KEYCLOAK_URL}${KEYCLOAK_URL_HTTP_RELATIVE_PATH}`;
-
-export const KEYCLOAK_URL_REALM =
-  window._env_ && window._env_.REACT_APP_KEYCLOAK_URL_REALM;
-export const KEYCLOAK_CLIENT =
-  window._env_ && window._env_.REACT_APP_KEYCLOAK_CLIENT;
 export const RSBC_API_URL =
   (window._env_ && window._env_.REACT_APP_RSBC_API_URL) ||
   "http://localhost:5100";
--- forms-flow-rsbcservice/src/endpoints/index.ts
+++ forms-flow-rsbcservice/src/endpoints/index.ts
@@ -1,14 +1,6 @@
 import { WEB_BASE_URL, RSBC_API_URL } from "./config";
 
 const API = {
-  GET_DASHBOARDS: `${WEB_BASE_URL}/dashboards`,
-  GET_GROUPS: `${WEB_BASE_URL}/groups`,
-  GET_ROLES: `${WEB_BASE_URL}/roles`,
-  GET_USERS: `${WEB_BASE_URL}/user`,
-  DASHBOARD_AUTHORIZATION: `${WEB_BASE_URL}/authorizations/dashboard`,
-  USER_ROLE_GROUP_PERMISSION: `${WEB_BASE_URL}/user/<user_id>/permission/groups/<group_id>`,
-  ADD_USER: `${WEB_BASE_URL}/user/add-user`,
-  GET_PERMISSIONS: `${WEB_BASE_URL}/roles/permissions`,
   GET_STATIC_DATA: `${RSBC_API_URL}/api/v1/static/<resource_name>`,
 };
 
