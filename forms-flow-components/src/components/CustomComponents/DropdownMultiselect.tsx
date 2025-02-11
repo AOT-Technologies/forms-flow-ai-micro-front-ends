b63926c4bf0deae72ce93104c648cace46842db6
fahad-aot <fahad.k@aot-technologies.com>
2025-02-11 00:58:37 -0800

    fixed sonar issues

--- forms-flow-components/src/components/CustomComponents/DropdownMultiselect.tsx
+++ forms-flow-components/src/components/CustomComponents/DropdownMultiselect.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { InputDropdown } from './InputDropdown';
 import { MultipleSelect } from './MultiSelect';
 
 interface DropdownMultiselectProps {
   dropdownLabel: string; 
-  defaultDropdownValue?: string | any; 
   onDropdownChange?: (value: any) => void; // Callback when dropdown changes
   onMultiSelectionChange?: (values: any[]) => void; // Callback when multi-selection changes
   multiSelectPillClassName?: string;
