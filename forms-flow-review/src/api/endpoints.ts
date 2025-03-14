c87cf68c90dc1a7f1a92f3bfead1d46da65cf802
Arun S <arun.s@aot-technologies.com>
2025-03-14 12:01:15 +0530

    FWF-4365 [Feature] Optimised endpoint imports

--- forms-flow-review/src/api/endpoints.ts
+++ forms-flow-review/src/api/endpoints.ts
@@ -1,8 +1,8 @@
-import { WEB_BASE_URL } from "./config";
+import { WEB_BASE_URL, BPM_URL } from "./config";
 
 const API = {
     SAMPLE:`${WEB_BASE_URL}/sample`,
-    BPM_BASE_URL_SOCKET_IO :`${window._env_?.REACT_APP_BPM_URL}/forms-flow-bpm-socket`
+    BPM_BASE_URL_SOCKET_IO :`${BPM_URL}/forms-flow-bpm-socket`
 }
 
 export default API;
