4c49400dcc040a2c09990e9a1df8cf7d323370e7
abilpraju-aot <abil.raju@aot-technologies.com>
2025-04-25 17:55:23 +0530

    FWF-4516: [Feature] Added submissions micro front end module

---
+++ forms-flow-submissions/src/root.component.tsx
@@ -0,0 +1,13 @@
+import Submissions from ".";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BASE_ROUTE } from "./constants";
+
+export default function Root(props) {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path={BASE_ROUTE} render={() => <Submissions props={props} />}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
