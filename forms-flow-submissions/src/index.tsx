789c3be7ff108523a8c873838c0638784cf9f1a1
Abil P Raju <83952803+abilpraju-aot@users.noreply.github.com>
2025-04-28 13:02:41 +0530

    FWF-4516: [Feature] Added submissions micro front end module (#549)

---
+++ forms-flow-submissions/src/index.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { Route, Switch, Redirect, useHistory, useParams } from "react-router-dom";
+import { KeycloakService, StorageService } from "@formsflow/service";
+import {
+  KEYCLOAK_URL_AUTH,
+  KEYCLOAK_URL_REALM,
+  KEYCLOAK_CLIENT,
+} from "./endpoints/config"; 
+import { BASE_ROUTE, MULTITENANCY_ENABLED } from "./constants";
+import i18n from "./config/i18n";
+import "./index.scss";
+import Loading from "./components/Loading";
+import SubmissionsList from "./Routes/SubmissionsListing/List";
+const authorizedRoles = new Set(["create_submissions","view_submissions"]);
+
+const Submissions = React.memo(({ props }: any) => {
+  const { publish, subscribe } = props;
+  const history = useHistory();
+  const { tenantId } = useParams();
+  const [instance, setInstance] = React.useState(props.getKcInstance());
+  const [isAuth, setIsAuth] = React.useState(instance?.isAuthenticated());
+  const [isClient, setClient] = React.useState(false);
+ 
+  const baseUrl = MULTITENANCY_ENABLED ? `/tenant/${tenantId}/` : "/";
+
+  React.useEffect(() => {
+    publish("ES_ROUTE", { pathname: `${baseUrl}submissions` });
+    subscribe("ES_CHANGE_LANGUAGE", (msg, data) => {
+      i18n.changeLanguage(data);
+    })
+  }, []);
+
+  React.useEffect(() => {
+    StorageService.save("tenantKey", tenantId || '')
+  }, [tenantId])
+
+  React.useEffect(() => {
+    if (!isAuth) {
+
+      let instance = KeycloakService.getInstance(
+        KEYCLOAK_URL_AUTH,
+        KEYCLOAK_URL_REALM,
+        KEYCLOAK_CLIENT,
+        tenantId
+      );
+      instance.initKeycloak(() => {
+        setIsAuth(instance.isAuthenticated());
+        publish("FF_AUTH", instance);
+      });
+    }
+  }, []);
+  React.useEffect(() => {
+    if (!isAuth) return
+    const roles = JSON.parse(StorageService.get(StorageService.User.USER_ROLE));
+    if (roles.some((role: any) => authorizedRoles.has(role))) {
+      setClient(true);
+    }
+    const locale = localStorage.getItem("i18nextLng")
+    if (locale) i18n.changeLanguage(locale);
+    publish("ES_ROUTE", { pathname: `${baseUrl}submissions` });
+    subscribe("ES_CHANGE_LANGUAGE", (msg, data) => {
+      i18n.changeLanguage(data);
+    })
+
+  }, [isAuth])
+
+
+  if (!isAuth) {
+    return <Loading />
+  }
+  if(!isClient) return <p>unauthorized</p>
+  return (
+    <>
+        <div className="main-container " tabIndex={0}>
+          <div className="container mt-5">
+            <div className="min-container-height ps-md-3">
+              <Switch>
+                <Route
+                  exact
+                  path={`${BASE_ROUTE}submissions`}
+                  render={() => <SubmissionsList {...props}/>}
+                />
+                <Redirect from="*" to="/404" />
+              </Switch>
+            </div>
+     
+          </div>
+        </div>
+   
+    </>
+  );
+});
+
+export default Submissions;
