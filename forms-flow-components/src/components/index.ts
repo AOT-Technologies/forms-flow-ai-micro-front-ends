1220255879b93ff45bf45d8d33224a07d1291e73
Ajay-aot <ajay.k@aot-technologies.com>
2025-09-25 16:14:24 +0530

    FWF-5203[feature] - textInput and textArea for v8

--- forms-flow-components/src/components/index.ts
+++ forms-flow-components/src/components/index.ts
@@ -40,3 +40,5 @@ export * from "./CustomComponents/VariableModal";
 export * from "./CustomComponents/FormComponent";
 export * from "./CustomComponents/StepperComponent";
 export * from "./CustomComponents/CustomButton";  
+export * from "./CustomComponents/CustomTextInput";
+export * from "./CustomComponents/CustomTextArea";
