27c619316a64d8896feeb82ca173fb075b399fdd
Rintu Merin <rintu.merin@aot-technologies.com>
2025-10-23 15:40:58 +0530

    FWF-5405: [Feature] Flow History Page  removed unwanted code

--- forms-flow-components/src/components/CustomComponents/HistoryPage.tsx
+++ forms-flow-components/src/components/CustomComponents/HistoryPage.tsx
@@ -1,6 +1,4 @@
 import React, { useState, useRef, useEffect } from "react";
-import { CustomButton } from "./Button";
-// import { CloseIcon } from "../SvgIcons/index";
 import { ConfirmModal } from "./ConfirmModal";
 import { useTranslation } from "react-i18next";
 import { HelperServices, StorageService, StyleServices } from "@formsflow/service";
@@ -15,17 +13,13 @@ interface HistoryPageProps {
   revertBtnAction: (cloneId: string | null) => void;
   loadMoreBtnAction: () => void;
   revertBtnText: string;
-  // loadMoreBtnText: string;
   revertBtndataTestid?: string;
   revertBtnariaLabel?: string;
-  // loadMoreBtndataTestId?: string;
-  // loadMoreBtnariaLabel?: string;
   allHistory: AllHistory[];
   categoryType: string;
   historyCount: number;
   currentVersionId?: number | string;
   disabledData: { key: string; value: any };
-  ignoreFirstEntryDisable?: boolean;
   disableAllRevertButton?: boolean;
   loading?: boolean;
   refreshBtnAction: () => void;
@@ -50,63 +44,17 @@ interface AllHistory {
   id?: string;
 }
 
-// const HistoryField = ({ fields }) => {
-//   return (
-//     <div className="details">
-//       {fields.map(({ id, heading, value }) => (
-//         <>
-//           {heading ? (
-//             <div key={id}>
-//               <p>{heading}</p>
-//               <p>{value}</p>
-//             </div>
-//           ) : (
-//             ""
-//           )}
-//         </>
-//       ))}
-//     </div>
-//   );
-// };
-
-// const RevertField = ({
-//   variant,
-//   size,
-//   label,
-//   onClick,
-//   dataTestId,
-//   ariaLabel,
-//   disabled = false,
-// }) => {
-//   return (
-//     <CustomButton
-//       disabled={disabled}
-//       label={label}
-//       onClick={onClick}
-//       dataTestId={dataTestId}
-//       ariaLabel={ariaLabel}
-//       actionTable
-//     />
-//   );
-// };
-
 export const HistoryPage: React.FC<HistoryPageProps> = React.memo(
   ({
     revertBtnAction,
-    // loadMoreBtnAction,
     revertBtnText,
-    // loadMoreBtnText,
     revertBtndataTestid = "revert-button",
     revertBtnariaLabel = "Revert Button",
-    // loadMoreBtndataTestId = "loadmore-button",
-    // loadMoreBtnariaLabel = "Loadmore Button",
     allHistory,
     categoryType,
     historyCount,
     disableAllRevertButton = false,
-    ignoreFirstEntryDisable = false,
     disabledData = { key: "", value: "" }, // we can pass the key and its value based on that we can disable revert button eg: key:"processKey",value:"bpmn" if the data[key] == value it will disable
-    // handleRefresh,
     loading = false,
     refreshBtnAction,
     handlePaginationModelChange,
@@ -138,14 +86,7 @@ export const HistoryPage: React.FC<HistoryPageProps> = React.memo(
         setClonedFormId(params.cloned_form_id);
         setProcessId(params.process_id);
       }
-      // onClose();
     };
-
-    // const handleClose = () => {
-    //   // onClose();
-    //   setHasLoadedMoreForm(false);
-    //   setHasLoadedMoreWorkflow(false);
-    // };
 
     const handleKeepLayout = () => {
       setShowConfirmModal(false);
@@ -180,49 +121,16 @@ export const HistoryPage: React.FC<HistoryPageProps> = React.memo(
     };
 
     useEffect(() => {
-      // adjustTimelineHeight();
-      // window.addEventListener("resize", adjustTimelineHeight);
-      // if (show) {
         if (!hasLoadedMoreForm && categoryType === "FORM") {
           setHasLoadedMoreForm(false);
         } else if (!hasLoadedMoreWorkflow && categoryType === "WORKFLOW") {
           setHasLoadedMoreWorkflow(false);
         }
-      // }
 
       return () => {
         window.removeEventListener("resize", adjustTimelineHeight);
       };
     }, [allHistory]);
-
-    // const handleLoadMore = () => {
-    //   loadMoreBtnAction();
-    //   if (categoryType === "FORM") {
-    //     setHasLoadedMoreForm(true);
-    //   } else if (categoryType === "WORKFLOW") {
-    //     setHasLoadedMoreWorkflow(true);
-    //   }
-    // };
-
-    // const renderLoadMoreButton = () => {
-    //   const shouldRenderButton =
-    //     (categoryType === "FORM" && !hasLoadedMoreForm) ||
-    //     (categoryType === "WORKFLOW" && !hasLoadedMoreWorkflow);
-
-    //   return shouldRenderButton ? (
-    //     <div className="load-more" ref={loadMoreRef}>
-    //       <CustomButton
-    //         label={loadMoreBtnText}
-    //         onClick={handleLoadMore}
-    //         dataTestId={loadMoreBtndataTestId}
-    //         ariaLabel={loadMoreBtnariaLabel}
-    //         secondary
-    //       />
-    //     </div>
-    //   ) : null;
-    // };
- 
-    // const paginationModel = { page: 0, pageSize: 10 };
 
     const generateRows = (historyData: AllHistory[]) => {
       return historyData.map((entry, index) => ({
@@ -311,132 +219,8 @@ export const HistoryPage: React.FC<HistoryPageProps> = React.memo(
       ];
     };
 
-    // const renderHistory = () => {
-    //   return allHistory.map((entry, index) => {
-    //     const isMajorVersion = entry.isMajor;
-    //     const version = `${entry.majorVersion}.${entry.minorVersion}`;
-    //     const cloned_form_id =
-    //       categoryType === "FORM" ? entry.changeLog.cloned_form_id : null;
-    //     const process_id = categoryType === "WORKFLOW" ? entry.id : null;
-    //     const isLastEntry = index === allHistory.length - 1;
-    //     const userRoles = JSON.parse(
-    //       StorageService.get(StorageService.User.USER_ROLE)
-    //     );
-    //     const isCreateDesigns = userRoles?.includes("create_designs");
-    //     const isViewDesigns = userRoles?.includes("view_designs");
-    //     const isReadOnly = isViewDesigns && !isCreateDesigns;
-    //     const revertButtonDisabled =
-    //       isReadOnly ||
-    //       disableAllRevertButton ||
-    //       entry[disabledData.key] == disabledData.value ||
-    //       (!ignoreFirstEntryDisable && index === 0);
-    //     const fields = [
-    //       {
-    //         id: 1,
-    //         heading: entry.publishedOn ? t("Published On") : "",
-    //         value: entry.publishedOn
-    //           ? HelperServices?.getLocalDateAndTime(entry.publishedOn)
-    //           : "",
-    //       },
-    //       {
-    //         id: 2,
-    //         heading: t("Saved On"),
-    //         value: HelperServices?.getLocalDateAndTime(entry.created),
-    //       },
-    //       { id: 3, heading: t("Saved By"), value: entry.createdBy },
-    //       ...(categoryType === "WORKFLOW"
-    //         ? [
-    //             {
-    //               id: 4,
-    //               heading: t("Type"),
-    //               value:
-    //                 entry.processType === "LOWCODE"
-    //                   ? "No-Code"
-    //                   : entry.processType,
-    //             },
-    //           ]
-    //         : []),
-    //     ];
-
-    //     return (
-    //       <React.Fragment key={`${entry.version}-${index}`}>
-    //         {isMajorVersion && (
-    //           <div
-    //             ref={isLastEntry ? lastEntryRef : null}
-    //             className={`version major ${
-    //               categoryType === "WORKFLOW" ? "workflow" : ""
-    //             }`}
-    //           >
-    //             <p className="heading">Version {version}</p>
-    //             <HistoryField fields={fields} />
-    //             <RevertField
-    //               variant="secondary"
-    //               size="sm"
-    //               disabled={revertButtonDisabled}
-    //               label={revertBtnText}
-    //               onClick={() =>
-    //                 handleRevertClick(version, cloned_form_id, process_id)
-    //               }
-    //               dataTestId={revertBtndataTestid}
-    //               ariaLabel={revertBtnariaLabel}
-    //             />
-    //           </div>
-    //         )}
-    //         {!isMajorVersion && (
-    //           <div
-    //             ref={isLastEntry ? lastEntryRef : null}
-    //             className={`version minor ${
-    //               categoryType === "WORKFLOW" ? "workflow" : ""
-    //             }`}
-    //           >
-    //             <p className="heading">Version {version}</p>
-    //             <HistoryField
-    //               key={`${entry.version}-${index}`}
-    //               fields={fields}
-    //             />
-    //             <RevertField
-    //               variant="secondary"
-    //               size="sm"
-    //               label={revertBtnText}
-    //               disabled={revertButtonDisabled}
-    //               onClick={() =>
-    //                 handleRevertClick(version, cloned_form_id, process_id)
-    //               }
-    //               dataTestId={revertBtndataTestid}
-    //               ariaLabel={revertBtnariaLabel}
-    //             />
-    //           </div>
-    //         )}
-    //       </React.Fragment>
-    //     );
-    //   });
-    // };
     return (
       <>
-        {/* <div
-          className="historypage-container"
-          data-testid="history-page"
-          aria-labelledby="history-page-title"
-          aria-describedby="history-page-message"
-        >
-          <div className="historypage-header">
-            <div className="historypage-title" id="history-page-title">
-              <p>{t(title)}</p>
-            </div>
-            <div className="icon-close" onClick={handleClose}>
-              <CloseIcon data-testid="close-icon" />
-            </div>
-          </div>
-          <div className="historypage-body">
-            <div className="history-content">
-              {historyCount > 0 && (
-                <div ref={timelineRef} className="timeline"></div>
-              )}
-              {renderHistory()}
-            </div>
-            {historyCount > 4 && renderLoadMoreButton()}
-          </div>
-        </div> */}
         <Paper sx={{ height: { sm: 400, md: 510, lg: 510 }, width: "100%" }}
           className="historypage-container"
           data-testid="history-page"
