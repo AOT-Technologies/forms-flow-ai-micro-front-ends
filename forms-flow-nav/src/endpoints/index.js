55b4eb0e00eec77ea84d221aa48f4f8a958ee327
DESKTOP-978J9P7\Dell <alex.antony@aot-technologies.com>
2026-01-29 00:31:01 -0800

    FWF-5979:[Feature] - User Details updation API integration

--- forms-flow-nav/src/endpoints/index.js
+++ forms-flow-nav/src/endpoints/index.js
@@ -3,7 +3,8 @@ import { WEB_BASE_URL, MT_ADMIN_BASE_URL, MT_ADMIN_BASE_URL_VERSION } from "./co
 const API = {
     LANG_UPDATE: `${WEB_BASE_URL}/user/locale`,
     GET_TENANT_DATA: `${MT_ADMIN_BASE_URL}/${MT_ADMIN_BASE_URL_VERSION}/tenant`,
-    INTEGRATION_ENABLE_DETAILS: `${WEB_BASE_URL}/integrations/embed/display`
+    INTEGRATION_ENABLE_DETAILS: `${WEB_BASE_URL}/integrations/embed/display`,
+    USER_PROFILE_UPDATE: `${WEB_BASE_URL}/user/<user_id>/profile`
 }
 
 export default API;
