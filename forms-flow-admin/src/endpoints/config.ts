69a1ccbf7dde0f23f56ebe14c921554d1900b5de
Ajay-aot <ajay.k@aot-technologies.com>
2026-02-24 23:00:43 +0530

    FWF-6170[feature] - Temporary user invite flow changes

--- forms-flow-admin/src/endpoints/config.ts
+++ forms-flow-admin/src/endpoints/config.ts
@@ -10,3 +10,7 @@ export const KEYCLOAK_URL_AUTH = `${KEYCLOAK_URL}${KEYCLOAK_URL_HTTP_RELATIVE_PA
 
 export const KEYCLOAK_URL_REALM = (window._env_ && window._env_.REACT_APP_KEYCLOAK_URL_REALM) 
 export const KEYCLOAK_CLIENT = (window._env_ && window._env_.REACT_APP_KEYCLOAK_CLIENT) 
+export const MT_ADMIN_BASE_URL = (window._env_ && window._env_.REACT_APP_MT_ADMIN_BASE_URL) 
+export const API_PROJECT_URL = (window._env_ && window._env_.REACT_APP_API_PROJECT_URL) 
+export const MT_ADMIN_BASE_URL_VERSION = (window._env_ && window._env_.REACT_APP_MT_ADMIN_BASE_URL_VERSION) 
+export const WEB_BASE_CUSTOM_URL = (window._env_ && window._env_.REACT_APP_WEB_BASE_CUSTOM_URL) || "" 
